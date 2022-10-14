5617cd302b485465ef0c5f205e5e3a7df591cf9d
Caio <contato.pycursos@gmail.com>
2022-10-13 21:16:40 -0300

    aula #06

---
+++ servicos/forms.py
@@ -0,0 +1,20 @@
+from django.forms import ModelForm
+from .models import Servico, CategoriaManutencao
+
+class FormServico(ModelForm):
+    class Meta:
+        model = Servico
+        exclude = ['finalizado', 'protocole']
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field in self.fields:
+            self.fields[field].widget.attrs.update({'class': 'form-control'})
+            self.fields[field].widget.attrs.update({'placeholder': field})
+
+        choices = list()
+        for i, j in self.fields['categoria_manutencao'].choices:
+            categoria = CategoriaManutencao.objects.get(titulo=j)
+            choices.append((i.value, categoria.get_titulo_display()))
+        
+        self.fields['categoria_manutencao'].choices = choices
--- servicos/views.py
+++ servicos/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
-
-# Create your views here.
-
+from .forms import FormServico
+from django.http import HttpResponse
 
 def novo_servico(request):
-    return render(request, 'novo_servico.html')
+    if request.method == "GET":
+        form = FormServico()
+        return render(request, 'novo_servico.html', {'form': form})
+    elif request.method == "POST":
+        form = FormServico(request.POST)
+
+        if form.is_valid():
+            form.save()
+            return HttpResponse('Salvo com sucesso')
+        else:
+            return render(request, 'novo_servico.html', {'form': form})
